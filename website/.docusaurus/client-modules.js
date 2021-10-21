33cbcfaca194920dd5b3041bfe0315c4c566f28a
yoshiyuki <brother-zhou@qq.com>
2021-10-21 10:11:57 +0800

    delete ignore for good

--- website/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/yoshiyuki/WebstormProjects/docs-off-chain-voting/website/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/yoshiyuki/WebstormProjects/docs-off-chain-voting/website/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/yoshiyuki/WebstormProjects/docs-off-chain-voting/website/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/yoshiyuki/WebstormProjects/docs-off-chain-voting/website/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/yoshiyuki/WebstormProjects/docs-off-chain-voting/website/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/Users/yoshiyuki/WebstormProjects/docs-off-chain-voting/website/src/css/custom.css"),
-];
